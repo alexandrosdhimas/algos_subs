c4b84f9e048404f74070429796226f4c956f670e
Alexandros Dimas <alexandrosdms98@gmail.com>
2021-11-16 12:34:49 +0200

    assignment 1 first commit

---
+++ Assign1/RandomWord.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        int i = 1;
+        String champion = "";
+        while (!StdIn.isEmpty())
+        {
+            if (StdRandom.bernoulli(1.0/i))
+                champion = StdIn.readString();
+            else
+                StdIn.readString(); // needed to exit loop
+            i++;
+        }
+        StdOut.println(champion);
+    }
+}
